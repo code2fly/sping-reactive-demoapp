033c4ffd215c23bf1b12650c091da69600b3d019
Mohit <Mohit.Pant6@t-mobile.com>
2019-03-31 15:18:28 +0300

    added webflux - entire reactive stack

--- src/main/java/com/goreactive/reactivedemo/controller/HelloController.java
+++ src/main/java/com/goreactive/reactivedemo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.goreactive.reactivedemo.controller;
 
 import com.goreactive.reactivedemo.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
@@ -11,12 +13,16 @@ import reactor.core.publisher.Flux;
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @RequestMapping("/employees")
     public Flux<Employee> getAllEmployees() {
+        logger.info("request recieved for employee");
         Flux<Employee> employeeFlux = employeeRepository.findAll();
+        logger.info("returning from employee controller call");
         return employeeFlux;
     }
 
